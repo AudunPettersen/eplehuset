8f2f4869531e718f78efc910d7467c9baa249c75
Audun Pettersen <audunpp@gmail.com>
2020-01-23 08:09:36 +0100

    3.0

--- eple.sql
+++ eple.sql
@@ -1,34 +1,70 @@
+DROP TABLE IF EXISTS kunde cascade;
+DROP TABLE IF EXISTS vare cascade;
+DROP TABLE IF EXISTS bestilling cascade;
+DROP TABLE IF EXISTS linje cascade;
+DROP TABLE IF EXISTS kategori cascade;
+
+
+CREATE TABLE users (
+    userid SERIAL PRIMARY KEY,
+    username text unique not null,
+    role text default 'user',
+    password text not null
+); 
+
+
 CREATE TABLE kunde (
-  kundeid serial primary key,
-  fornavn text not null,
-  etternavn text not null,
-  adresse text not null,
-  epost text not null,
-  tlf text not null,
-  kjonn text
+  kundeid SERIAL PRIMARY KEY,
+  fornavn text NOT NULL,
+  etternavn text NOT NULL,
+  adresse text,
+  epost text,
+  tlf text,
+  kjonn text,
+  userid int unique not null
 );
 
-CREATE bruker
 
-CREATE TABLE ordre (
-  ordreid serial primary key,
-  ordredato text not null,
-  betalt text default 'false' check (
-    betalt = 'true'
-    or betalt = 'false'
-  ),
-  kundeid int references kunde (kundeid)
+CREATE TABLE  vare  (
+   vareid  SERIAL PRIMARY KEY,
+   varenavn  text unique NOT NULL,
+   pris int default 0,
+   kategoriid int not null
 );
 
-CREATE TABLE eksemplar (
-  eksemplarid serial primary key,
-  tilstand text,
-  ordreid int references ordre (ordreid),
-  produktid int references produkt (produktid)
+
+CREATE TABLE  bestilling  (
+   bestillingid  SERIAL PRIMARY KEY,
+   dato  date NOT NULL,
+   kundeid int NOT NULL
 );
 
-CREATE TABLE produkt (
-  produktid serial primary key,
-  vare text not null,
-  utgivelse date
+
+CREATE TABLE  linje  (
+   linjeid  SERIAL PRIMARY KEY,
+   antall  int DEFAULT 1,
+   vareid  int NOT NULL,
+   bestillingid  int NOT NULL
 );
+
+CREATE TABLE kategori (
+kategoriid SERIAL PRIMARY KEY,
+kategorinavn text unique NOT NULL
+);
+
+
+
+ALTER TABLE  bestilling  ADD FOREIGN KEY ( kundeid ) REFERENCES  kunde  ( kundeid );
+ALTER TABLE  linje  ADD FOREIGN KEY ( bestillingid ) REFERENCES  bestilling  ( bestillingid );
+ALTER TABLE  linje  ADD FOREIGN KEY ( vareid ) REFERENCES  vare  ( vareid );
+ALTER TABLE  kunde  ADD FOREIGN KEY ( userid ) REFERENCES  users  ( userid );
+ALTER TABLE  vare ADD FOREIGN KEY ( kategoriid ) REFERENCES kategori (kategoriid);
+
+
+
+alter table bestilling owner to eple;
+alter table vare owner to eple;
+alter table kunde owner to eple;
+alter table linje owner to eple;
+alter table users owner to eple;
+alter table kategori owner to eple;
